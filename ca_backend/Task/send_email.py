0b1d664c0e53e2731fb7b4a4b4406e54bd82be41
mradulsingh0223 <mradul.singh.min21@itbhu.ac.in>
2024-01-16 01:08:50 +0530

    [UPDATED] Populate Verification Email Tokens for empty users

--- ca_backend/Task/send_email.py
+++ ca_backend/Task/send_email.py
@@ -1,16 +1,16 @@
 import smtplib 
+import os
 from decouple import config
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
-import os
 
 try:
     SUPPORT_EMAIL = config("SUPPORT_EMAIL")
 except:
     SUPPORT_EMAIL = "tech@technex.in"
 
-LOGO_FILE_PATH = "./ca_backend/logo/asset.png"
+LOGO_FILE_PATH = os.path.abspath("./ca_backend/logo/asset.png")
 
 with open(LOGO_FILE_PATH, 'rb') as fp:
     IMAGE = MIMEImage(fp.read())
